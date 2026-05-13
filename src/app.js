9de762ffbd602e31a8396a4227e8d4c4ccca1cbf
Aman Athghara <amanathghara@gmail.com>
2026-05-13 21:21:43 +0530

    fixes again

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.set('trust proxy', 1);
 
 // app.use(cors(corsOptions));
 app.use(cors());
-app.options('*', cors());
+// app.options('*', cors());
 
 // app.options('/*splat', cors(corsOptions));
 
